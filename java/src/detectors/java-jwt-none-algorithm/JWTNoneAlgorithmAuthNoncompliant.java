d0f233f69de83623530b4463db24f36c90dc0a6a
Devashish [C] Rane <branedev@amazon.com>
2024-11-15 19:03:44 +0530

    moved import inside fact annot

--- java/src/detectors/java-jwt-none-algorithm/JWTNoneAlgorithmAuthNoncompliant.java
+++ java/src/detectors/java-jwt-none-algorithm/JWTNoneAlgorithmAuthNoncompliant.java
@@ -1,9 +1,10 @@
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
 // {fact rule=java-jwt-none-algorithm@v1.0 defects=1}
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
 public class JWTNoneAlgorithmAuthNoncompliant {
     // Noncompliant: Uses the insecure `none` algorithm for JWT, which allows token forgery and bypasses signature verification.
     public static String nonCompliant(){
