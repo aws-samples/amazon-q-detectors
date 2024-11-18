d84573ec55864a4eedc70583c340df4d89bb4192
atonaamz <104926752+atonaamz@users.noreply.github.com>
2024-11-18 02:24:43 -0800

    fix Compliant typo

--- terraform/src/detectors/terraform-unencryption-not-prevented-by-athena/terraform-unencryption-not-prevented-by-athena-complaint.tf
+++ terraform/src/detectors/terraform-unencryption-not-prevented-by-athena/terraform-unencryption-not-prevented-by-athena-complaint.tf
@@ -6,7 +6,7 @@ resource "aws_athena_workgroup" "example" {
   name = "example-workgroup"
 
   configuration {
-    # Complaint: The Athena workgroup prevents unencryption.
+    # Compliant: The Athena workgroup prevents unencryption.
     enforce_workgroup_configuration    = true
     result_configuration {
       output_location = "s3://example-bucket/athena-results"
