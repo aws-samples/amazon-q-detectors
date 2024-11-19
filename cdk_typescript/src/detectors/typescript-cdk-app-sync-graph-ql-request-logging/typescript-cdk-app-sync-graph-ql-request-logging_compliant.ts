32978a9f2411607891ab41ba1a77879ca6ed0449
Shreya [C] Bhagat <shrebhaa@amazon.com>
2024-11-19 19:05:25 +0530

    Fixed the indentation issues

--- cdk_typescript/src/detectors/typescript-cdk-app-sync-graph-ql-request-logging/typescript-cdk-app-sync-graph-ql-request-logging_compliant.ts
+++ cdk_typescript/src/detectors/typescript-cdk-app-sync-graph-ql-request-logging/typescript-cdk-app-sync-graph-ql-request-logging_compliant.ts
@@ -7,14 +7,14 @@ import * as cdk from 'aws-cdk-lib';
 import { Stack } from 'aws-cdk-lib';
 
 export class CdkStarterStack extends cdk.Stack {
-	constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);      
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
     // Compliant: Configures logging with CloudWatch for monitoring API access and invalid requests.
     new CfnGraphQLApi(Stack, 'rGraphqlApi', {
       authenticationType: 'AMAZON_COGNITO_USER_POOL',
       name: 'foo',
       logConfig: { cloudWatchLogsRoleArn: 'foo' }
     });
-	}
+  }
 }
 // {/fact}
--- cdk_typescript/src/detectors/typescript-cdk-app-sync-graph-ql-request-logging/typescript-cdk-app-sync-graph-ql-request-logging_non-compliant.ts
+++ cdk_typescript/src/detectors/typescript-cdk-app-sync-graph-ql-request-logging/typescript-cdk-app-sync-graph-ql-request-logging_non-compliant.ts
@@ -7,14 +7,14 @@ import * as cdk from 'aws-cdk-lib';
 import { Stack } from 'aws-cdk-lib';
 
 export class CdkStarterStack extends cdk.Stack {
-	constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-		super(scope, id, props);      
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
     // Noncomplaint: Missing request-level logging, reducing visibility into API access and usage.
     new CfnGraphQLApi(Stack, 'rGraphqlApi', {
       authenticationType: 'AMAZON_COGNITO_USER_POOL',
       name: 'foo',
       logConfig: { excludeVerboseContent: true }
     });
-	}
+  }
 }
 // {/fact}
