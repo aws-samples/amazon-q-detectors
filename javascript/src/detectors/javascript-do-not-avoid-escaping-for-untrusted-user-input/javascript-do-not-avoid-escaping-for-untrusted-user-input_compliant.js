a307b9a946fb3d4c445a8bc5fa77a19d0a44d028
Akshata Chaudhari <akschauo@amazon.com>
2024-12-03 17:28:18 +0530

    Added 10 DL cases for javascript.

---
+++ javascript/src/detectors/javascript-do-not-avoid-escaping-for-untrusted-user-input/javascript-do-not-avoid-escaping-for-untrusted-user-input_compliant.js
@@ -0,0 +1,42 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+// {fact rule=javascript-do-not-avoid-escaping-for-untrusted-user-input@v1.0 defects=0}
+const csrf = require('csurf');
+const cookieParser = require('cookie-parser');
+const Handlebars = require('handlebars');
+const express = require("express");
+const fetch = require('node-fetch');
+
+const app = express();
+
+app.use(cookieParser());
+
+const csrfProtection = csrf();
+app.use(csrfProtection);
+
+function compliant() {
+    return fetch("link")
+        .then(response => response.json()) // Parse JSON response
+        .then(data => Handlebars.escapeExpression(data)) // Escape the data
+        .catch(error => {
+            console.error("Error in compliant function:", error);
+            throw error; // Propagate the error
+        });
+}
+
+app.get('/secure', (req, res) => {
+    compliant()
+        .then(sanitizedData => {
+            res.json({ data: sanitizedData });
+        })
+        .catch(error => {
+            res.status(500).send("An error occurred while processing your request.");
+        });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+// {/fact}
