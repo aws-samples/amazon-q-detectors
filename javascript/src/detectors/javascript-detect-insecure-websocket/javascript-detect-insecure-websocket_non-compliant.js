ba6da1273bdadd61e3abf61bb0a75541838fcc5f
aastha <aastjais@amazon.com>
2024-12-26 19:01:24 +0530

    Corrected typo

--- javascript/src/detectors/javascript-detect-insecure-websocket/javascript-detect-insecure-websocket_non-compliant.js
+++ javascript/src/detectors/javascript-detect-insecure-websocket/javascript-detect-insecure-websocket_non-compliant.js
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT-0
 
 // {fact rule=javascript-detect-insecure-websocket@v1.0 defects=1}
-// NonComplioant: Use of the wss:// scheme for WebSocket connections, which securely encrypts the transmitted data.
+// NonCompliant: Use of the wss:// scheme for WebSocket connections, which securely encrypts the transmitted data.
 var scheme   = "ws://";
 var uri      = scheme + window.document.location.host + "/";
 var ws       = new WebSocket(uri);
