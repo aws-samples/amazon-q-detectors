b7b00f4e8611889c931c576cff47e9d5be4ced82
Akshata Chaudhari <akschauo@amazon.com>
2025-07-16 14:51:57 +0530

    Resolve PR comments on go detector library cases.

--- golang/src/detectors/go-subproc/go-subproc_compliant.go
+++ golang/src/detectors/go-subproc/go-subproc_compliant.go
@@ -7,11 +7,21 @@ package main
 import (
 	"net/http"
 	"os/exec"
+	"regexp"
 )
 
 func compliant(w http.ResponseWriter, r *http.Request) {
-	// Compliant: Hardcoded command prevents command injection attacks.
-	cmd := exec.Command("cat", "/var/log/app.log")
+
+	userInput := r.URL.Query().Get("filename")
+	
+	re := regexp.MustCompile(`^[a-zA-Z0-9_\-\.]+$`)
+	if !re.MatchString(userInput) {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
+	
+	// Compliant: Use a hardcoded command with sanitized input.
+	cmd := exec.Command("cat", "/var/log/"+userInput)
 	output, err := cmd.Output()
 	if err != nil {
 		http.Error(w, "Command failed", http.StatusInternalServerError)
